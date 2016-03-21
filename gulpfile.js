060531e5a394e9846a79bd0f476c6f81daa4adc1
Nikita Pchelintsev <nikita@x-cart.com>
2016-03-21 18:01:53 +0300

    Fix typo in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var insert = require('gulp-insert');
 gulp.task('default', function() {
     return gulp.src('flexy-to-twig.jison')
         .pipe(jison({ moduleType: 'commonjs' }))
-        .pipe(insert.prepend('#!/usr/bin/env node\nn'))
+        .pipe(insert.prepend('#!/usr/bin/env node\n'))
         .pipe(gulp.dest('.'));
 });

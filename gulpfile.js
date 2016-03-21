d641fb79aa7926820d1b9bc1f5941a85a0d02c8b
Nikita Pchelintsev <nikita@x-cart.com>
2016-03-21 23:12:25 +0300

    Separate flexy-to-twig module and the binary

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var jison = require('gulp-jison');
 var insert = require('gulp-insert');
+var rename = require('gulp-rename');
  
 gulp.task('default', function() {
     return gulp.src('flexy-to-twig.jison')
         .pipe(jison({ moduleType: 'commonjs' }))
+        .pipe(gulp.dest('.'))
         .pipe(insert.prepend('#!/usr/bin/env node\n'))
+        .pipe(rename('flexy-to-twig-bin.js'))
         .pipe(gulp.dest('.'));
 });
